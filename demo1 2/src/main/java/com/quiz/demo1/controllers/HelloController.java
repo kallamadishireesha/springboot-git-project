d424930e4fe26a76b8f21ea13bcc3add9ebbbcf4
kallamadishireesha <kallamadishireesha@gmail.com>
2024-09-24 11:04:57 +0530

    Add files via upload

---
+++ demo1 2/src/main/java/com/quiz/demo1/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.quiz.demo1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(defaultValue = "World") String name, Model model) {
+        model.addAttribute("user", name);
+        return "welcome";  //welcome.html
+    }
+}
